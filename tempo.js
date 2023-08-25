109c641ed58df5a3b1fc57c87377c9921f3d11e1
mesqta <meskita@proton.me>
2023-08-25 18:12:26 -0300

    first commit

---
+++ tempo.js
@@ -0,0 +1,21 @@
+setInterval(()=>{
+    const tempo = document.querySelector("#tempo");
+    let date = new Date();
+    let hora = date.getHours();
+    let minutos = date.getSecounds();
+    let dia_noche = "AM";
+    if(hora >12){
+        dia_noche = "PM";
+        hora = hora -12;
+    }
+    if(segundos <10){
+        segundos = "0" + segundos;
+    }
+    if(minutos <10){
+        minutos = "0" + minutos;
+    }
+    if(hora <10){
+        hora = "0" + hora;
+    }
+    tempo.textContent = hora + ":" + minutos+ ":" +segundos + " " + dia_noche;
+});
